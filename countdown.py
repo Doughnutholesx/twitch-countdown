2e22d491a0cc8f37ec0c500fbac137cb5751df06
smonaghan90 <sean.monaghan@lottomart.com>
2020-11-04 14:54:52 +0000

    Timer for twitch

---
+++ countdown.py
@@ -0,0 +1,24 @@
+import time
+
+# define the countdown func.
+FILE_NAME = 'countdown.txt'
+def countdown(t):
+
+    while t:
+        with open(FILE_NAME,'w') as f:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            f.write(timer + "\n")
+            f.close()
+            print(timer)
+            time.sleep(1)
+            t -= 1
+
+    print('Lets gooo!!')
+
+
+# input time in seconds
+t = input("Enter the time in seconds: ")
+
+# function call
+countdown(int(t))
